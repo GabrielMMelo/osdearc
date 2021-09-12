905fa88b621d1447717e08c64206abe07225caef
Gabriel De Melo <gabrielmelocomp@gmail.com>
2021-09-12 15:03:40 -0300

    fix: add all docker-compose files to scripts

--- bin/start_all.sh
+++ bin/start_all.sh
@@ -4,4 +4,14 @@ echo "Rebuilding..."
 
 docker compose build -q
 
+docker compose -f docker-compose-airbyte.yml build -q
+
+docker compose -f docker-compose-superset.yml build -q
+
+echo "Starting..."
+
 docker compose up -d
+
+docker compose -f docker-compose-airbyte.yml up -d
+
+docker compose -f docker-compose-superset.yml up -d
--- bin/stop_all.sh
+++ bin/stop_all.sh
@@ -1,1 +1,5 @@
 docker compose down
+
+docker compose -f docker-compose-airbyte.yml down
+
+docker compose -f docker-compose-superset.yml down
